40c2417cd92b60e1ed2bebd7f9bee1f7bbcd1284
Daniel Koohmarey <daniel.koohmarey@berkeley.edu>
2013-04-23 19:33:00 -0700

    Adding testStrategy
    Can now test strategies simply in terminal by running:
    python testStrategy.py *strategy goes here*
    i.e python testStrategy.py meanReversion

---
+++ quant/testStrategy.py
@@ -0,0 +1,21 @@
+import quantPy as qp
+import ysq
+import wIndicators as wI
+import argparse
+
+def runStrategy(strategy):
+  if strategy == 'meanReversion':
+		qp.spPicker(wI.meanReversion)
+	elif strategy == 'roundNumbers':
+		qp.spPicker(wI.roundNumbers)
+	else:
+		print('Invalid Strategy selected. Current valid strategies: meanReversion, roundNumbers')
+
+def main():
+	parser = argparse.ArgumentParser(description='process strategy')
+	parser.add_argument('strategy', help='Strategy name you want to test', default='meanReversion', nargs='?')
+	args = parser.parse_args()
+	runStrategy(args.strategy)
+
+if __name__ == "__main__":
+    main()
