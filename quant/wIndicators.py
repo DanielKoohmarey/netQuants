732e452f960bbb11686e96834d43562b6e8b44c9
Daniel Koohmarey <daniel.koohmarey@berkeley.edu>
2013-04-23 19:34:06 -0700

    Creating round numbers strategy

--- quant/wIndicators.py
+++ quant/wIndicators.py
@@ -19,6 +19,17 @@ def meanReversion(priceSeries, period = 5, mulFactor = 1, addFactor = 0):
     frame['signal'][0:(period-1)] = 0
 
     return frame['signal']
+    
+    
+def roundNumbers(priceSeries, period = 5, mulFactor = 1, addFactor = 0):
+    frame = pd.DataFrame(priceSeries)
+    frame.columns = ['price']
+    frame['change'] = priceSeries.diff()
+    frame['sd'] = pd.rolling_std(frame['price'], period)
+    frame['signal'] = np.where(frame['change'] < (mulFactor*frame['sd']+addFactor), 1, -1)
+    frame['signal'][0:(period-1)] = 0
+
+    return frame['signal']
 
 def maCrossover(priceSeries, period = 3):
     return 0
