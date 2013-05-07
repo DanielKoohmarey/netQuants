1337c4348b4d6883fcebb0ad1f16bad4f09b8491
Daniel Koohmarey <daniel.koohmarey@berkeley.edu>
2013-05-06 17:19:49 -0700

    Update wIndicators.py

--- quant/wIndicators.py
+++ quant/wIndicators.py
@@ -19,17 +19,6 @@ def meanReversion(priceSeries, period = 5, mulFactor = 1, addFactor = 0):
     frame['signal'][0:(period-1)] = 0
 
     return frame['signal']
-    
-    
-def roundNumbers(priceSeries, period = 5, mulFactor = 1, addFactor = 0):
-    frame = pd.DataFrame(priceSeries)
-    frame.columns = ['price']
-    frame['change'] = priceSeries.diff()
-    frame['sd'] = pd.rolling_std(frame['price'], period)
-    frame['signal'] = np.where(frame['change'] < (mulFactor*frame['sd']+addFactor), 1, -1)
-    frame['signal'][0:(period-1)] = 0
-
-    return frame['signal']
 
 lastOpen = 0.0
 def roundNumbers(priceSeries,stock = 'goog'):
