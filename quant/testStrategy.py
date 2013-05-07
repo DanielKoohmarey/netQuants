d91cfaf2ac4de6521b2782ad007f1d69e5e9bc87
Daniel Koohmarey <daniel.koohmarey@berkeley.edu>
2013-05-06 17:28:56 -0700

    Update testStrategy.py

--- quant/testStrategy.py
+++ quant/testStrategy.py
@@ -4,15 +4,7 @@ import wIndicators as wI
 import argparse
 
 def runStrategy(strategy):
-<<<<<<< HEAD
-<<<<<<< HEAD
 	if strategy == 'meanReversion':
-=======
-  if strategy == 'meanReversion':
->>>>>>> 732e452f960bbb11686e96834d43562b6e8b44c9
-=======
-  if strategy == 'meanReversion':
->>>>>>> 732e452f960bbb11686e96834d43562b6e8b44c9
 		qp.spPicker(wI.meanReversion)
 	elif strategy == 'roundNumbers':
 		qp.spPicker(wI.roundNumbers)
@@ -26,12 +18,4 @@ def main():
 	runStrategy(args.strategy)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-<<<<<<< HEAD
     main()
-=======
-    main()
->>>>>>> 732e452f960bbb11686e96834d43562b6e8b44c9
-=======
-    main()
->>>>>>> 732e452f960bbb11686e96834d43562b6e8b44c9
